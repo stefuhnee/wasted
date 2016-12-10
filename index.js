f43d0bebefd7656bc6b6913aaa47386f2304837d
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-09 23:22:41 -0800

    command line now takes flags and parses them appropriately

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
 
+const filePath = process.argv[2] && !process.argv[2].startsWith('-') ? process.argv[2] : __dirname;
+const args = filePath === __dirname ? process.argv.slice(2) : process.argv.slice(3);
 
-require('./lib/file-finder').getFiles(process.argv[2] ? process.argv[2] : __dirname);
+require('./lib/file-finder').getFiles(filePath, args);
