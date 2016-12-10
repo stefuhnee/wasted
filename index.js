1068b42423497041d1af8d6a9e4c62d7db527ba6
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-09 19:08:38 -0800

    working on stylizing print out

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 
-require('./lib/file-finder')(process.argv[2] ? process.argv[2] : __dirname);
+require('./lib/file-finder').getFiles(process.argv[2] ? process.argv[2] : __dirname);
