9c20b50318261080f65c0f4d652a7a3b13437396
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-11 19:42:05 -0800

    sort-desc test finished

---
+++ test/sort-descending-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const sortDesc = require('../lib/sort-descending');
+
+describe('testing sort-descending', function() {
+
+  it('should sort an array of objects in descending order by size', () => {
+    let input = [{size: 34}, {size: 2}, {size: 12}, {size: 200}];
+    let expected = [{size: 200}, {size: 34}, {size: 12}];
+
+    expect(sortDesc(input)).to.deep.equal(expected);
+  });
+});
