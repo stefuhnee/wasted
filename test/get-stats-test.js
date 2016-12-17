9c0efb3a28c67add8f042f358dd777f43c8583f3
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-17 14:28:13 -0800

    get stats test finished

--- test/get-stats-test.js
+++ test/get-stats-test.js
@@ -5,8 +5,17 @@ const getStats = require('../lib/get-stats');
 
 describe('testing #get-stats', function() {
 
-  it('should get stats for a file and add the path', () => {
-
-    expect(sortDesc(input)).to.deep.equal(expected);
+  it('should get stats for a file and add the path', (done) => {
+    getStats(`${__dirname}/data/test.txt`)
+    .then(result => {
+      console.log(result);
+      expect(result).to.have.property('mode');
+      expect(result).to.have.property('size');
+      expect(typeof result.size).to.equal('number');
+      expect(result).to.have.property('path');
+      expect(result.path.startsWith(__dirname)).to.equal(true);
+      done();
+    })
+    .catch(console.log);
   });
 });
--- test/sort-descending-test.js
+++ test/sort-descending-test.js
@@ -7,7 +7,7 @@ describe('testing #sort-descending', function() {
 
   it('should sort an array of objects in descending order by size', () => {
     let input = [{size: 34}, {size: 2}, {size: 12}, {size: 200}];
-    let expected = [{size: 200}, {size: 34}, {size: 12}];
+    let expected = [{size: 200}, {size: 34}, {size: 12}, {size: 2}];
 
     expect(sortDesc(input)).to.deep.equal(expected);
   });
