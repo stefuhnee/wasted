f4817d3dae0895808238c5616ad1ab45f5a4b4f6
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-17 14:37:49 -0800

    testing error handling

--- test/get-stats-test.js
+++ test/get-stats-test.js
@@ -8,14 +8,20 @@ describe('testing #get-stats', function() {
   it('should get stats for a file and add the path', (done) => {
     getStats(`${__dirname}/data/test.txt`)
     .then(result => {
-      console.log(result);
       expect(result).to.have.property('mode');
       expect(result).to.have.property('size');
       expect(typeof result.size).to.equal('number');
       expect(result).to.have.property('path');
       expect(result.path.startsWith(__dirname)).to.equal(true);
       done();
-    })
-    .catch(console.log);
+    });
+  });
+
+  it('should catch errors', (done) => {
+    getStats(`invalid path`)
+    .catch(err => {
+      expect(err).to.not.equal(null);
+      done();
+    });
   });
 });
