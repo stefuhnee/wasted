1761c12d2153b77e0424bf8c35f5673bcc3abc05
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-19 10:46:26 -0800

    fixed test wording for clarity

--- test/flatten-file-array-test.js
+++ test/flatten-file-array-test.js
@@ -6,7 +6,7 @@ const flattenFileArray = require('../lib/flatten-file-array');
 describe('testing #flatten-file-array', function() {
   let input = [[1, 2], [3], [4, 5, 6, 7]];
 
-  it('should take a nested array and flatten it', () => {
+  it('should take a nested array and flatten it (one level)', () => {
     expect(flattenFileArray(input)).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
   });
 
