cee9e29373af7a445fdde2f367867b8e63753a4a
Stefanie Hansen <stefuhnee@gmail.com>
2016-12-19 10:45:58 -0800

    flatten file array test done

---
+++ test/flatten-file-array-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const expect = require('chai').expect;
+const flattenFileArray = require('../lib/flatten-file-array');
+
+describe('testing #flatten-file-array', function() {
+  let input = [[1, 2], [3], [4, 5, 6, 7]];
+
+  it('should take a nested array and flatten it', () => {
+    expect(flattenFileArray(input)).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+});
