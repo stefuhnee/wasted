cea70f20761784e074b63f12d2c82e167edbe643
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-01 16:43:39 -0800

    updated version

--- bin/wasted.bin.js
+++ bin/wasted.bin.js
@@ -4,6 +4,6 @@
 
 const filePath = process.argv[2] && !process.argv[2].startsWith('-') ? process.argv[2] : __dirname;
 
-const args = filePath === __dirname ? process.argv.slice(2) : process.argv.slice(3);
+const args = filePath === process.env.PWD ? process.argv.slice(2) : process.argv.slice(3);
 
 require('../lib/file-finder')(filePath, args);
