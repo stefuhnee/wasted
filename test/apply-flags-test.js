e898d042cfbb947381c64ff3ccfcd5afc4b136e3
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-01 23:13:44 -0800

    extracted recursion from apply flags and added directly into file finder functionality

--- test/apply-flags-test.js
+++ test/apply-flags-test.js
@@ -6,19 +6,13 @@ const applyFlags = require('../lib/apply-flags');
 describe('testing #apply-flags', function() {
   let input = [{type: 'Directory'}, {type: 'File'}];
 
-  it('should filter out directories by default', (done) => {
-    applyFlags(input, '')
-    .then(result => {
-      expect(result.length).to.equal(1);
-      done();
-    });
+  it('should filter out directories by default', () => {
+    let result = applyFlags(input, '');
+    expect(result.length).to.equal(1);
   });
 
-  it('should take an array of file objects, and a string a flags and return a modified array of objects depending on the flag', (done) => {
-    applyFlags(input, '-d')
-    .then(result => {
-      expect(result.length).to.equal(2);
-      done();
-    });
+  it('should take an array of file objects, and a string a flags and return a modified array of objects depending on the flag', () => {
+    let result = applyFlags(input, '-d');
+    expect(result.length).to.equal(2);
   });
 });
